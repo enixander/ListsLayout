ca7a5e6a95cac2496fb79ce86a9d54079a87dad4
enixander <xandergorsh@gmail.com>
2019-10-10 23:24:50 +0300

    Implemented simple numerical list view

--- app/src/main/java/com/github/enixander/listslayout/MainActivity.kt
+++ app/src/main/java/com/github/enixander/listslayout/MainActivity.kt
@@ -2,11 +2,21 @@ package com.github.enixander.listslayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numericalListView.setData(
+            arrayOf(
+                getString(R.string.lorem_ipsum_40),
+                getString(R.string.lorem_ipsum_20),
+                getString(R.string.lorem_ipsum_40),
+                getString(R.string.lorem_ipsum_20)
+            )
+        )
     }
 }
